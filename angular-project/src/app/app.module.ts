b6493c9b0d2295a494e96df8e8d1bd5dd9c72736
Ahkalamiammiam <vianneytraina@gmail.com>
2019-09-18 22:24:41 +0200

    Add Angular Animation to The Routed Component

--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -23,6 +24,7 @@ import { ProductsModule } from './products/products.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     NgbModule,
     ArticlesModule,
     ProductsModule,
